1a777bec5f8fde2cec1c2d76d453ddb56c27ca92
Karl Hepworth <karl.hepworth@gmail.com>
2020-05-26 21:12:13 +1000

    action overrides
    Signed-off-by: Karl Hepworth <karl.hepworth@gmail.com>

--- main.go
+++ main.go
@@ -33,12 +33,16 @@ var (
 	registryDestination string
 
 	configFile           string
-	diffAction           bool
 	dry                  bool
 	imageSet             string
 	namespaceDestination string
 	tagSource            string
 	tagDestiantion       string
+
+	diffAction  bool
+	pullAction  bool
+	pushAction  bool
+	retagAction bool
 )
 
 // retag will apply a new tag to an existing image reference.
@@ -51,11 +55,13 @@ func retag(origin, result string) {
 	}
 
 	fmt.Println("docker tag", origin, result)
-	if !dry {
-		if e := cli.ImageTag(ctx, origin, result); e != nil {
-			fmt.Println(e)
-			os.Exit(1)
-		}
+	if dry && !retagAction {
+		return
+	}
+
+	if e := cli.ImageTag(ctx, origin, result); e != nil {
+		fmt.Println(e)
+		os.Exit(1)
 	}
 }
 
@@ -69,11 +75,13 @@ func push(result string) {
 	}
 
 	fmt.Println("docker push", result)
-	if !dry {
-		if _, e := cli.ImagePush(ctx, result, types.ImagePushOptions{}); e != nil {
-			fmt.Println(e)
-			os.Exit(1)
-		}
+	if dry && !pushAction {
+		return
+	}
+
+	if _, e := cli.ImagePush(ctx, result, types.ImagePushOptions{}); e != nil {
+		fmt.Println(e)
+		os.Exit(1)
 	}
 }
 
@@ -102,14 +110,14 @@ func pull(image, tag string) {
 
 	ref := fmt.Sprintf("%v/%v:%v", registrySource, image, tag)
 	fmt.Println("docker pull", ref)
-	if !dry {
-		if _, e := cli.ImagePull(ctx, ref, types.ImagePullOptions{}); e != nil {
-			fmt.Println(e)
-			os.Exit(1)
-		} else {
-			fmt.Println(e)
-			os.Exit(1)
-		}
+
+	if dry && !pullAction {
+		return
+	}
+
+	if _, e := cli.ImagePull(ctx, ref, types.ImagePullOptions{}); e != nil {
+		fmt.Println(e)
+		os.Exit(1)
 	}
 }
 
@@ -192,11 +200,15 @@ func config() {
 func main() {
 
 	flag.StringVar(&configFile, "config", "config.yml", "Path to configuration file")
-	flag.BoolVar(&diffAction, "diff", false, "In the cases where dry-run is enabled, also run the diff")
 	flag.BoolVar(&dry, "dry-run", false, "Do not perform any actions, just report the expected actions")
 	flag.StringVar(&imageSet, "set", "", "Run the workload against the specified image-set")
 	flag.StringVar(&tagSource, "source", "", "Source tag to identify or pull before processing")
 	flag.StringVar(&tagDestiantion, "destination", "", "Destination tag to push to")
+
+	flag.BoolVar(&diffAction, "diff", false, "In the cases where dry-run is enabled, also run the diff action")
+	flag.BoolVar(&pullAction, "pull", false, "In the cases where dry-run is enabled, also run the pull action")
+	flag.BoolVar(&pushAction, "push", false, "In the cases where dry-run is enabled, also run the push action")
+	flag.BoolVar(&retagAction, "retag", false, "In the cases where dry-run is enabled, also run the retag action")
 
 	flag.Parse()
 
